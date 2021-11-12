bbd9fd1e685b3bab005fe1aceab99a4c0bb9cbed
Fatih Erikli <fatih-erikli@outlook.com>
2021-11-12 21:19:16 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechGrammarList =
+      window.SpeechGrammarList || window.webkitSpeechGrammarList;
+
+    const recognition = new SpeechRecognition();
+    const grammar = new SpeechGrammarList();
+    grammar.addFromString(`
+      #JSGF V1.0;
+      <greeting>= (guten tag) {hi};
+    `);
+    const speechRecognitionList = new SpeechGrammarList();
+    recognition.grammars = speechRecognitionList;
+    recognition.interimResults = true;
+    recognition.continuous = true;
+    recognition.lang = "de-DE";
+
+    recognition.onresult = function (event) {
+      let finalTranscript = "";
+      let interimTranscript = "";
+
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          finalTranscript += event.results[i][0].transcript;
+        } else {
+          interimTranscript += event.results[i][0].transcript;
+        }
+      }
+
+      setResults([finalTranscript || interimTranscript]);
+    };
+
+    recognition.start();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      Deutsch Live Transcription
+      <br />
+      {results.map((result, index) => (
+        <div key={index}>{result}</div>
+      ))}
     </div>
   );
 }
